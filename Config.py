f5fbb3460e596ef157f88ac450ed79452650b123
ErdemViaypi <122641197+ErdemViaypi@users.noreply.github.com>
2023-01-21 16:14:39 +0300

    Create Config.py

---
+++ Config.py
@@ -0,0 +1,28 @@
+import os
+
+class Config():
+
+    # Bu dəyərləri my.telegram.org saytından əldə edin
+
+    #>>> https://my.telegram.org
+
+    API_ID = "14993791"
+
+    API_HASH = "60a2897e2d7de8eefd6cf610085b1f0a"
+
+    BOT_TOKEN = "5877068414:AAEuYMN7WLfoAXZ7r5310Ob8AWDHPUg-Lug"
+
+    BOT_USERNAME = "ustaggerbot"
+
+    BOT_NAME = "ustaggerbot"
+
+    BOT_ID = "5877068414"
+
+    SUDO_USERS = "5601145914"
+
+    SUPPORT_CHAT = "ustaggersup"
+
+    OWNER_ID = "5601145914"
+
+    OWNER_USERNAME = "goruntulemesayisi31"
+
